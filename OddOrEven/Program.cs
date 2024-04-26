60cebe39348d0a01dfab2511987996991ad804d3
tanelmetsar <tanel.metsar@tktk.ee>
2024-04-26 22:12:01 +0300

    add week3 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+Console.WriteLine("Enter a number:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+// kas kasutaja arv on paaris või paaritu
+
+int result = userNum % 2;
+
+if (result != 0)
+{
+    Console.WriteLine("User number is odd.");
+}
+else
+{
+    Console.WriteLine("User number is even.");
+}
